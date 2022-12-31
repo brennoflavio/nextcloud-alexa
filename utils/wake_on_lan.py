e1d8ce2f704860e3116f9e5442b1283a55f2a822
brennoflavio <contato@brennoflavio.com.br>
2022-12-31 16:32:40 -0300

    implement SleepOnLanIntent

--- utils/wake_on_lan.py
+++ utils/wake_on_lan.py
@@ -4,3 +4,7 @@ import os
 
 def wake_on_lan():
     subprocess.run(os.getenv("WAKEONLAN_COMMAND"), shell=True)
+
+
+def sleep_on_lan():
+    subprocess.run(os.getenv("SLEEPONLAN_COMMAND"), shell=True)
